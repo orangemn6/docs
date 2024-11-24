8d9403bc8d8b25a1aa3ebee92ff46633a5f59b86
orangemn6 <orangemn6@protonmail.com>
2024-11-24 01:09:21 -0500

    vault backup: 2024-11-24 01:09:20

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: {
-      provider: "plausible",
+      provider: "null",
     },
     locale: "en-US",
     baseUrl: "docs.jacobg.co",
